59b0353e98ef87a984400714d77f31812ef1c856
Arsene Awounou <developer.awounou@gmail.com>
2023-01-29 22:20:04 +0100

    guess input done

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,7 +1,29 @@
 import React from "react";
 
-function GuessInput() {
-  return <div />;
+function GuessInput({ guess, setGuess }) {
+  return (
+    <form 
+      onSubmit={e => {
+        e.preventDefault();
+        if (guess.length != 5) {
+          window.alert('Your guess should be 5 characters long');
+          return
+        }
+        console.log({ guess });
+        setGuess('');
+      }}
+      className="guess-input-wrapper">
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input 
+        type="text"
+        required={true}
+        min='5'
+        max='5'
+        value={guess}
+        onChange={e => setGuess(e.target.value.toUpperCase())}
+        id="guess-input" type="text" />
+    </form>
+  );
 }
 
 export default GuessInput;
