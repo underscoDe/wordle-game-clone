9fbb19efd24d090e0f40d95fce0f12f8aad128a5
Arsene Awounou <developer.awounou@gmail.com>
2023-01-29 23:31:46 +0100

    inputs tracker

---
+++ src/components/Guesses/Guesses.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Guesses() {
+  return (
+    <div className="guess-results">
+      <p className="guess">FIRST</p>
+      <p className="guess">GUESS</p>
+    </div>
+  );
+}
+
+export default Guesses;
