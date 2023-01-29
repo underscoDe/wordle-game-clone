006f2cfecc84bb83a761bee710a016d2695dedec
Arsene Awounou <developer.awounou@gmail.com>
2023-01-29 22:21:00 +0100

    remove duplicated type attr

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -2,26 +2,28 @@ import React from "react";
 
 function GuessInput({ guess, setGuess }) {
   return (
-    <form 
-      onSubmit={e => {
+    <form
+      onSubmit={(e) => {
         e.preventDefault();
         if (guess.length != 5) {
-          window.alert('Your guess should be 5 characters long');
-          return
+          window.alert("Your guess should be 5 characters long");
+          return;
         }
         console.log({ guess });
-        setGuess('');
+        setGuess("");
       }}
-      className="guess-input-wrapper">
+      className="guess-input-wrapper"
+    >
       <label htmlFor="guess-input">Enter guess:</label>
-      <input 
+      <input
         type="text"
         required={true}
-        min='5'
-        max='5'
+        min="5"
+        max="5"
         value={guess}
-        onChange={e => setGuess(e.target.value.toUpperCase())}
-        id="guess-input" type="text" />
+        onChange={(e) => setGuess(e.target.value.toUpperCase())}
+        id="guess-input"
+      />
     </form>
   );
 }
