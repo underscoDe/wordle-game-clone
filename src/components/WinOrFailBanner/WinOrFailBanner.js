9fbd2f833926b8439f47e6efea11b9006e0c4870
Arsene Awounou <developer.awounou@gmail.com>
2023-01-31 18:24:01 +0100

    wordle game

--- src/components/WinOrFailBanner/WinOrFailBanner.js
+++ src/components/WinOrFailBanner/WinOrFailBanner.js
@@ -8,7 +8,7 @@ function WinOrFailBanner({ gameStatus: status, answer, numberOfGuesses }) {
           <p>
             <strong>Congratulations!</strong> Got it in{" "}
             <strong>
-              {numberOfGuesses === 1 ? "1 guess" : `${numberOfGuesses} guesses`}{" "}
+              {numberOfGuesses === 1 ? "1 guess" : `${numberOfGuesses} guesses`}
             </strong>
             .
           </p>
